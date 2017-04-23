1ed20186af52bc900c8346d35f6e478f26fd1cbd
AlexB44 <a.bodier130@gmail.com>
2017-04-24 00:27:00 +0200

    js has been cleaned

--- js/original_gallery_filter.js
+++ js/original_gallery_filter.js
@@ -2,18 +2,18 @@ var filters = document.querySelectorAll('.setFilter')
 var works = document.querySelectorAll('.work')
 
 filters.forEach(function(f) {
-	f.addEventListener('click', function(event) {
-  	applyFilter(event.target.textContent.toLowerCase(), works)
-  })
+    f.addEventListener('click', function(event) {
+        applyFilter(event.target.textContent.toLowerCase(), works)
+    })
 })
 
 function applyFilter(filter, selectors) {
-	selectors.forEach(function(s) {
-  	s.classList.remove('disabled')
-    if (!s.classList.contains(filter)) {
-    	s.classList.add('disabled')
-    }
-  })
+    selectors.forEach(function(s) {
+        s.classList.remove('disabled')
+        if (!s.classList.contains(filter)) {
+            s.classList.add('disabled')
+        }
+    })
 }
 
-applyFilter('print', works)
+applyFilter('illustration', works)
